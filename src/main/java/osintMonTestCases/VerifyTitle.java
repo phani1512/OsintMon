fa20dc80c62a4a9a16711aa5ffe5d1c5b84cfd04
phani1512 <phaneendraphani20@gmail.com>
2020-10-02 18:27:31 +0530

    Initial commit and push

---
+++ src/main/java/osintMonTestCases/VerifyTitle.java
@@ -0,0 +1,33 @@
+package osintMonTestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Selenium.OsintMon.Constaints;
+
+
+public class VerifyTitle {
+	public class Title extends Constaints {
+
+		@Test(priority = 0)
+		public void verifyPageTitle() throws InterruptedException {
+			test = extent.createTest("verifyPageTitle");
+			// Actual title
+			String Title = driver.getTitle().trim();
+			System.out.println("The page title is :" + Title);
+		//	String Expected_title = "Welcome to OSINTMon";
+			String Expected_title = "Welcome to OSINTM";
+			Assert.assertEquals(Title, Expected_title);
+			System.out.println("Title verified");
+
+		}
+//		@Test
+//		public void verifyHomePage() {
+//			test = extent.createTest("verifyHomePage");
+//			String HomeTitle = driver.getTitle();
+//			System.out.println("The Home page title is :" + HomeTitle);
+//			Assert.assertEquals(HomeTitle, "");
+//		}
+	}
+
+}
